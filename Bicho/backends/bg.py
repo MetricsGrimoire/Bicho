b9166ef90802800219cc90ff6de2ece758b277f2
Juan Francisco Gato Luis <jfcogato@libresoft.es>
2010-12-20 11:27:33 +0100

    added author

--- Bicho/backends/bg.py
+++ Bicho/backends/bg.py
@@ -15,6 +15,7 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #
 # Authors: Daniel Izquierdo Cortazar <dizquierdo@gsyc.es>
+#          Juan Francisco Gato Luis <jfcogato@libresoft.es>
 #
 
 
