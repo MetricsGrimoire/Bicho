365915f7dddabb28e1bd115e778c466ec8c513f9
Santiago Dueñas <sduenas@libresoft.es>
2011-03-08 18:16:10 +0100

    Add modular support for database engines

---
+++ Bicho/db/mysql.py
@@ -0,0 +1,232 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2007-2011  GSyC/LibreSoft, Universidad Rey Juan Carlos
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+# Authors: Santiago Dueñas <sduenas@libresoft.es>
+#
+
+"""
+MySQL database module
+"""
+
+from Bicho.db.database import *
+
+class DBMySQL(DBDatabase):
+    """
+    MySQL database adapter.
+    """
+
+    def __init__(self):
+        opts = Config()
+
+        self.database = create_database('mysql://' + opts.db_user_out + ':'
+                                        + opts.db_password_out + '@'
+                                        + opts.db_hostname_out + ':'
+                                        + opts.db_port_out + '/'
+                                        + opts.db_database_out)
+        self.store = Store(self.database)
+
+        self.create_tables([DBSupportedTracker, DBTrackerMySQL, DBPeopleMySQL,
+                            DBIssueMySQL, DBIssueRelationshipMySQL,
+                            DBCommentMySQL, DBAttachmentMySQL, DBChangeMySQL])
+
+
+class DBSupportedTracker(DBSupportedTracker):
+    """
+    MySQL subclass of L{DBSupportedTracker}.
+    """
+    __sql_table__ = 'CREATE TABLE IF NOT EXISTS supported_trackers ( \
+                     id INTEGER NOT NULL AUTO_INCREMENT, \
+                     name VARCHAR(64) NOT NULL, \
+                     version VARCHAR(64) NOT NULL, \
+                     PRIMARY KEY(id), \
+                     UNIQUE KEY(name, version) \
+                     )'
+
+
+class DBTrackerMySQL(DBTracker):
+    """
+    MySQL subclass of L{DBTracker}.
+    """
+    __sql_table__ = 'CREATE TABLE IF NOT EXISTS trackers ( \
+                     id INTEGER NOT NULL AUTO_INCREMENT, \
+                     url VARCHAR(255) NOT NULL, \
+                     type INTEGER NOT NULL, \
+                     retrieved_on DATETIME NOT NULL, \
+                     PRIMARY KEY(id), \
+                     UNIQUE KEY(url), \
+                     FOREIGN KEY(type) \
+                       REFERENCES tracker_types (id) \
+                         ON DELETE CASCADE \
+                         ON UPDATE CASCADE \
+                     )'
+
+
+class DBPeopleMySQL(DBPeople):
+    """
+    MySQL subclass of L{DBPeople}.
+    """
+    __sql_table__ = 'CREATE TABLE IF NOT EXISTS people ( \
+                     id INTEGER NOT NULL AUTO_INCREMENT, \
+                     name VARCHAR(64) NULL, \
+                     email VARCHAR(64) NULL, \
+                     user_id VARCHAR(64) NOT NULL, \
+                     tracker_id INTEGER NOT NULL, \
+                     PRIMARY KEY(id), \
+                     UNIQUE KEY(user_id, tracker_id), \
+                     INDEX people_tracker_idx(tracker_id), \
+                     FOREIGN KEY(tracker_id) \
+                       REFERENCES trackers(id) \
+                         ON DELETE CASCADE \
+                         ON UPDATE CASCADE \
+                     )'
+
+
+class DBIssueMySQL(DBIssue):
+    """
+    MySQL subclass of L{DBIssue}.   
+    """
+    __sql_table__ = 'CREATE TABLE IF NOT EXISTS issues ( \
+                     id INTEGER NOT NULL AUTO_INCREMENT, \
+                     tracker_id INTEGER NOT NULL, \
+                     issue VARCHAR(255) NOT NULL, \
+                     type VARCHAR(32) NULL, \
+                     summary VARCHAR(255) NOT NULL, \
+                     description TEXT NOT NULL, \
+                     status VARCHAR(32) NOT NULL, \
+                     resolution VARCHAR(32) NULL, \
+                     priority VARCHAR(32) NULL, \
+                     submitted_by INTEGER UNSIGNED NOT NULL, \
+                     submitted_on DATETIME NOT NULL, \
+                     assigned_to INTEGER UNSIGNED NOT NULL, \
+                     PRIMARY KEY(id), \
+                     UNIQUE KEY(issue, tracker_id), \
+                     INDEX issues_submitted_idx(submitted_by), \
+                     INDEX issues_assigned_idx(assigned_to), \
+                     INDEX issues_tracker_idx(tracker_id), \
+                     FOREIGN KEY(submitted_by) \
+                       REFERENCES people(id) \
+                         ON DELETE SET NULL \
+                         ON UPDATE CASCADE, \
+                     FOREIGN KEY(assigned_to) \
+                       REFERENCES people(id) \
+                         ON DELETE SET NULL \
+                         ON UPDATE CASCADE, \
+                     FOREIGN KEY(tracker_id) \
+                       REFERENCES trackers(id) \
+                         ON DELETE CASCADE \
+                         ON UPDATE CASCADE \
+                     )'
+
+
+class DBIssueRelationshipMySQL(DBIssueRelationship):
+    """
+    MySQL subclass of L{DBIssueRelationship}.
+    """
+    __sql_table__ = 'CREATE TABLE IF NOT EXISTS related_to ( \
+                     issue_id INTEGER UNSIGNED NOT NULL, \
+                     related_to INTEGER UNSIGNED NOT NULL, \
+                     type INTEGER UNSIGNED NOT NULL, \
+                     PRIMARY KEY(issue_id, related_to), \
+                     INDEX issues_related_idx1(issue_id), \
+                     INDEX issues_related_idx2(related_to), \
+                     FOREIGN KEY(issue_id) \
+                       REFERENCES issues(id) \
+                         ON DELETE CASCADE \
+                         ON UPDATE CASCADE, \
+                     FOREIGN KEY(related_to) \
+                       REFERENCES issues(id) \
+                         ON DELETE CASCADE \
+                         ON UPDATE CASCADE \
+                     )'
+
+
+class DBCommentMySQL(DBComment):
+    """
+    MySQL subclass of L{DBComment}.
+    """
+    __sql_table__ = 'CREATE TABLE IF NOT EXISTS comments ( \
+                     id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT, \
+                     issue_id INTEGER UNSIGNED NOT NULL, \
+                     comment_id INTEGER UNSIGNED, \
+                     text TEXT NOT NULL, \
+                     submitted_by INTEGER UNSIGNED NOT NULL, \
+                     submitted_on DATETIME NOT NULL, \
+                     PRIMARY KEY(id), \
+                     INDEX comments_submitted_idx(submitted_by), \
+                     INDEX comments_issue_idx(issue_id), \
+                     FOREIGN KEY(submitted_by) \
+                       REFERENCES people(id) \
+                         ON DELETE SET NULL \
+                         ON UPDATE CASCADE, \
+                     FOREIGN KEY(issue_id) \
+                       REFERENCES issues(id) \
+                         ON DELETE CASCADE \
+                         ON UPDATE CASCADE \
+                     )'
+
+
+class DBAttachmentMySQL(DBAttachment):
+    """
+    MySQL subclass of L{DBAttachment}.
+    """
+    __sql_table__ = 'CREATE TABLE IF NOT EXISTS attachments ( \
+                     id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT, \
+                     issue_id INTEGER UNSIGNED NOT NULL, \
+                     name VARCHAR(64) NOT NULL, \
+                     description TEXT NOT NULL, \
+                     url VARCHAR(255) NOT NULL, \
+                     submitted_by INTEGER UNSIGNED, \
+                     submitted_on DATETIME, \
+                     PRIMARY KEY(id), \
+                     INDEX attachments_submitted_idx(submitted_by), \
+                     INDEX attachments_issue_idx(issue_id), \
+                     FOREIGN KEY(submitted_by) \
+                       REFERENCES people(id) \
+                         ON DELETE SET NULL \
+                         ON UPDATE CASCADE, \
+                     FOREIGN KEY(issue_id) \
+                       REFERENCES issues(id) \
+                         ON DELETE CASCADE \
+                         ON UPDATE CASCADE \
+                     )'
+
+
+class DBChangeMySQL(DBChange):
+    """
+    MySQL subclass of L{DBChange}.
+    """
+    __sql_table__ = 'CREATE TABLE IF NOT EXISTS changes ( \
+                     id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT, \
+                     issue_id INTEGER UNSIGNED NOT NULL, \
+                     field VARCHAR(64) NOT NULL, \
+                     old_value VARCHAR(255) NOT NULL, \
+                     new_value VARCHAR(255) NOT NULL, \
+                     changed_by INTEGER UNSIGNED NOT NULL, \
+                     changed_on DATETIME NOT NULL, \
+                     PRIMARY KEY(id), \
+                     INDEX changes_issue_idx(issue_id), \
+                     INDEX changes_changed_idx(changed_by), \
+                     FOREIGN KEY(issue_id) \
+                       REFERENCES issues(id) \
+                         ON DELETE CASCADE \
+                         ON UPDATE CASCADE, \
+                     FOREIGN KEY(changed_by) \
+                       REFERENCES people(id) \
+                         ON DELETE SET NULL \
+                         ON UPDATE CASCADE \
+                    )'
