0bd7282adf5374665d75fa5fc71436bac3a73a5a
Luis Cañas Díaz <lcanas@libresoft.es>
2011-06-24 17:15:16 +0200

    Update information about project in Debian files and NEWS

--- Bicho/info.py
+++ Bicho/info.py
@@ -1,7 +1,7 @@
 PACKAGE = "bicho"
-VERSION = "0.4"
+VERSION = "0.9beta"
 
-AUTHOR = "GSyC/LibreSoft"
+AUTHOR = "GSyC/LibreSoft, Universidad Rey Juan Carlos"
 AUTHOR_EMAIL = "libresoft-tools-devel@lists.morfeo-project.org"
-COPYRIGHT = "Copyright (C) 2011 GSyC/LibreSoft, Universidad Rey Juan Carlos"
+COPYRIGHT = "Copyright (C) 2007-2011 GSyC/LibreSoft, Universidad Rey Juan Carlos"
 DESCRIPTION = "An analysis tool for issue tracking systems"
