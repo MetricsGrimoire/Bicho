1deaa29af70786193633cfaa19a33b3098e49be1
Alvaro del Castillo <acs@barrapunto.com>
2012-09-05 07:59:27 +0200

    Completed donwloading testing

--- tests/test_allura.py
+++ tests/test_allura.py
@@ -1,4 +1,4 @@
-import errno, os, sys, unittest, urllib
+import errno, json, os, pprint, sys, unittest, urllib
 sys.path.insert(0, "..")
 from Bicho.backends import Backend
 from Bicho.Config import Config
@@ -37,17 +37,16 @@ class AlluraTest(unittest.TestCase):
                 f = open(self.project_issues_file)
         return f
     
-    def read_issue(self):
-        issue_url = Config.url+"/"+str(self.issue_id_test)
-        issue_number = self.issue_id_test
+    def read_issue(self, issue_id):
+        issue_url = Config.url+"/"+str(issue_id)
         project_name = Config.url.split("/")[-2]                 
-        issue_file = os.path.join(self.tests_data_dir, project_name+"." + str(issue_number))
+        issue_file = os.path.join(self.tests_data_dir, project_name+"." + str(issue_id))
          
-        try:
-            print ("Test file " + issue_file)             
+        try:             
             f = open(issue_file)
         except Exception, e:
             if e.errno == errno.ENOENT:
+                print ("Downloading " + issue_file)
                 f = open(issue_file,'w')
                 fr = urllib.urlopen(issue_url)
                 f.write(fr.read())
@@ -67,6 +66,7 @@ class AlluraTest(unittest.TestCase):
 
         if os.path.isfile(changes_file): pass
         else:
+            print ("Downloading " + changes_file)
             f = open(changes_file,'w')
             fr = urllib.urlopen(changes_url)
             f.write(fr.read())
@@ -74,10 +74,13 @@ class AlluraTest(unittest.TestCase):
         return changes_file
 
     def testReadIssues(self):
-        self.read_issues()
+        issuesList_data = json.loads(self.read_issues().read())
+        for issue in issuesList_data['tickets']:
+            issue_data = self.read_issue(issue['ticket_num'])
+            changes_file = self.read_issue_changes(issue['ticket_num'])
         
     def testReadIssue(self): 
-        self.read_issue()
+        self.read_issue(self.issue_id_test)
         
     def testReadChange(self):
         self.read_issue_changes(self.issue_id_test)
