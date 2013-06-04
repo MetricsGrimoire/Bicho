0a843f86c18d7c0d449c96edb77e886170d4e2e4
Santiago Dueñas <sduenas@libresoft.es>
2013-06-04 15:03:43 +0200

    [BackendManager] New implementation of Backend and BackendManager
    This framework has been taken from Greenwitch project. It's based on
    the code from the book Rytis Sileika's "Pro Python
    System Administration".

--- Bicho/backends/__init__.py
+++ Bicho/backends/__init__.py
@@ -1,9 +1,11 @@
-# Copyright (C) 2011 GSyC/LibreSoft, Universidad Rey Juan Carlos
+# -*- coding: utf-8 -*-
 #
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
+# Copyright (C) 2011-2013 GSyC/LibreSoft, Universidad Rey Juan Carlos
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
 #
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -12,52 +14,111 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #
-# Authors: Daniel Izquierdo Cortazar <dizquierdo@gsyc.escet.urjc.es>
+# Authors:
+#         Santiago Dueñas <sduenas@libresoft.es>
 #
+
+"""
+Bicho backends framework.
+
+.. note::
+
+   This framework has been taken from Greenwitch project. It's based on
+   the code from the book Rytis Sileika's 'Pro Python System Administration'.
+"""
 
 import os
-import glob
-
-__all__ = ['Backend']
-
-class BackendUnknownError (Exception):
-    '''Unkown engine type'''
+import sys
 
 
-class Backend:
+class BackendImportError(Exception):
+    """Exception raised when an error occurs importing a backend."""
 
-    _backends = {}
+    def __init__(self, name, error=None):
+        if error is not None and not isinstance(error, Exception):
+            raise TypeError('expected type Exception in error parameter.')
+        self.name = name
+        self.error = error
 
-    @staticmethod
-    def register_backend (backend_name, backend_class):
-        Backend._backends[backend_name] = backend_class
+    def __str__(self):
+        msg = 'error importing backend %s.' % self.name
+        if self.error is not None:
+            msg += ' %s' % repr(self.error)
+        return msg
 
-    @staticmethod
-    def _get_backend (backend_name):
-        if backend_name not in Backend._backends:
+
+class BackendDoesNotExist(Exception):
+    """Exception raised when a backend is not found."""
+
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return 'backend %s not found.' % self.name
+
+
+class Backend(object):
+    """Abstract class for backends."""
+
+    def __init__(self, name):
+        self._name = name
+
+    @property
+    def name(self):
+        return self._name
+
+
+class BackendManager(object):
+    """Class for managing backends."""
+
+    def __init__(self, path=None):
+        self._testpath = path or os.path.dirname(__file__)
+        self._backends = {}
+        self._load_backends()
+
+    @property
+    def backends(self):
+        return self._backends.keys()
+
+    @property
+    def path(self):
+        return self._testpath
+
+    def get(self, name):
+        try:
+            return self._backends[name]
+        except KeyError:
+            raise BackendDoesNotExist(name)
+
+    def _load_backends(self):
+        backends = self._find_backends()
+        for backend in backends:
+            b = backend()
+            self._backends[b.name] = b
+
+    def _find_backends(self):
+        filenames = [fn for fn in os.listdir(self._testpath)]
+        files = [fn for fn in filenames \
+                 if os.path.isfile(os.path.join(self._testpath, fn)) \
+                 and fn.endswith('.py')]
+        candidates = [c.split('.py')[0] for c in files]
+        backends = self._import_backends(candidates)
+        return backends
+
+    def _import_backends(self, candidates):
+        if not self._testpath in sys.path:
+            sys.path.append(self._testpath)
+
+        for candidate in candidates:
             try:
-                __import__ ('Bicho.backends.%s' % backend_name)
-            except ImportError:
-                raise
+                __import__(candidate)
+            except Exception, e:
+                raise BackendImportError(candidate, e)
 
-        if backend_name not in Backend._backends:
-            raise BackendUnknownError ('Backend type %s not registered' % backend_name)
-    
-        return Backend._backends[backend_name]
-    
-    @staticmethod
-    def create_backend (backend_name):
-        backend_class = Backend._get_backend (backend_name)
-        return backend_class ()
-    
-    @staticmethod
-    def get_all_backends ():
-        # we should clean this directory
-        backends = []
-        not_backends = ('HTMLParser.py', 'HTMLUtils.py', '__init__.py')
-        for fname in glob.glob( os.path.join(os.path.dirname(__file__), '*.py') ):
-            if os.path.basename(fname) not in not_backends:
-                backends.append(os.path.basename(fname))
+        # Checks whether the backends were imported from the
+        # given modules
+        backends = [b for b in Backend.__subclasses__()
+                    if b.__module__ in candidates]
         return backends
