75ea55c6ca6b24e554b02f2d8be3de786cce7fd2
Sumana Harihareswara <sumanah@panix.com>
2013-11-06 23:27:32 -0500

    Ported Config.py from optparse to argparse
    Also:
    * moved positional-vs-keyword argument logic and precedence
      into argument setters
    * updated imports of modules
    * changed Config to a new-style class
    * updated Copyright date
    * removed extraneous whitespace

--- Bicho/Config.py
+++ Bicho/Config.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright (C) 2011 GSyC/LibreSoft, Universidad Rey Juan Carlos
+# Copyright (C) 2013 GSyC/LibreSoft, Universidad Rey Juan Carlos
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -19,14 +19,12 @@
 #        Carlos Garcia Campos <carlosgc@libresoft.es>
 #        Luis Cañas Díaz <lcanas@libresoft.es>
 #        Alvaro del Castillo <acs@bitergia.com>
-#
-# We should migrate to argparse. optparse is deprecated since Python 2.7
+#        Sumana Harihareswara <sumanah@panix.com>
 
 from backends import Backend
 import info
-from optparse import OptionGroup, OptionParser
+from argparse import ArgumentParser
 import os
-import pprint
 import sys
 from urllib2 import Request, urlopen, urlparse, URLError, HTTPError
 
@@ -45,7 +43,7 @@ class InvalidConfig(Exception):
     pass
 
 
-class Config:
+class Config(object):
 
     @staticmethod
     def load_from_file(config_file):
@@ -57,7 +55,7 @@ class Config:
             raise ErrorLoadingConfig("Error reading config file %s (%s)" % (\
                     config_file, str(e)))
 
-    @staticmethod        
+    @staticmethod
     def load():
         # FIXME: a hack to avoid circular dependencies. 
         from utils import bicho_dot_dir, printout
@@ -92,10 +90,9 @@ class Config:
         """
         """
         Config.check_params(['url','backend'])
-        
+
         if Config.backend + ".py" not in Backend.get_all_backends():
             raise InvalidConfig('Backend "' + Config.backend + '" does not exist')
-
 
         url = urlparse.urlparse(Config.url)
         check_url = urlparse.urljoin(url.scheme + '://' + url.netloc,'')
@@ -108,120 +105,119 @@ class Config:
                                + str(e.msg) + '(' + str(e.code) + ')')
         except URLError, e:
             raise InvalidConfig('We failed to reach a server. ' + str(e.reason))
-        
+
         except ValueError, e:
             print ("Not an URL: " + Config.url)
-            
-                
+
+
         if vars(Config).has_key('input') and Config.input == 'db':
             Config.check_params(['db_driver_in', 'db_user_in', 'db_password_in',
-                                 'db_hostname_in', 'db_port_in', 'db_database_in'])        
+                                 'db_hostname_in', 'db_port_in', 'db_database_in'])
         if vars(Config).has_key('output') and Config.output == 'db':
             Config.check_params(['db_driver_out','db_user_out','db_password_out',
                                  'db_hostname_out','db_port_out','db_database_out'])
-        
+
     @staticmethod
     def clean_empty_options(options):
-        clean_opt = {};        
+        clean_opt = {};
         for option in vars(options):
             if (vars(options)[option] is not None) and \
               ( not Config.__dict__.has_key(option)) :
                 clean_opt[option] = vars(options)[option]                    
         return clean_opt
-        
+
     @staticmethod
     def set_config_options(usage):
         """
         """
-        
-        parser = OptionParser(usage=usage, description=info.DESCRIPTION,
-                              version=info.VERSION)
+
+        parser = ArgumentParser(description=info.DESCRIPTION, conflict_handler='resolve')
+        # Conflict handler is to ensure reconciliation between
+        # keyword arguments and positional arguments for backend & url
+        parser.add_argument('--version', action='version', version=info.VERSION)
 
         # General options
-        parser.add_option('-b', '--backend', dest='backend',
-                          help='Backend used to fetch issues', default=None)
-        parser.add_option('--backend-user', dest='backend_user',
-                          help='Backend user', default=None)
-        parser.add_option('--backend-password', dest='backend_password',
-                          help='Backend password', default=None)
-        parser.add_option('-c', '--cfg', dest='cfgfile', 
-                          help='Use a custom configuration file', default=None)
-        parser.add_option('-d', '--delay', type='int', dest='delay',
-                          help='Delay in seconds betweeen petitions to avoid been banned',
-                          default='5')
-        parser.add_option('-g', '--debug', action='store_true', dest='debug',
-                          help='Enable debug mode', default=False)
-        parser.add_option('--gerrit-project', dest='gerrit_project',
-                          help='Project to be analyzed (gerrit backend)',
-                          default=None)
-        parser.add_option('-i', '--input', choices=['url', 'db'],
-                          dest='input', help='Input format', default='url')
-        parser.add_option('-o', '--output', choices=['db'],
-                          dest='output', help='Output format', default='db')
-        parser.add_option('-p', '--path', dest='path',
-                          help='Path where downloaded URLs will be stored',
-                          default=None)
-        parser.add_option('-u', '--url', dest='url',
-                          help='URL to get issues from using the backend',
-                          default=None)
-        parser.add_option('-l', '--logtable', action='store_true', dest='logtable',
-                          help='Enable generation of issues log table',
-                          default=False)
-    
+        parser.add_argument('-b', '--backend', dest='backend',
+                            help='Backend used to fetch issues', default=None)
+        parser.add_argument('--backend-user', dest='backend_user',
+                            help='Backend user', default=None)
+        parser.add_argument('--backend-password', dest='backend_password',
+                            help='Backend password', default=None)
+        parser.add_argument('-c', '--cfg', dest='cfgfile',
+                            help='Use a custom configuration file', default=None)
+        parser.add_argument('-d', '--delay', type='int', dest='delay',
+                            help='Delay in seconds betweeen petitions to avoid been banned',
+                            default='5')
+        parser.add_argument('-g', '--debug', action='store_true', dest='debug',
+                            help='Enable debug mode', default=False)
+        parser.add_argument('--gerrit-project', dest='gerrit_project',
+                            help='Project to be analyzed (gerrit backend)',
+                            default=None)
+        parser.add_argument('-i', '--input', choices=['url', 'db'],
+                            dest='input', help='Input format', default='url')
+        parser.add_argument('-o', '--output', choices=['db'],
+                            dest='output', help='Output format', default='db')
+        parser.add_argument('-p', '--path', dest='path',
+                            help='Path where downloaded URLs will be stored',
+                            default=None)
+        parser.add_argument('-u', '--url', dest='url',
+                            help='URL to get issues from using the backend',
+                            default=None)
+        parser.add_argument('-l', '--logtable', action='store_true',
+                            dest='logtable',
+                            help='Enable generation of issues log table',
+                            default=False)
+        # Positional arguments should override keyword arguments.
+        # This is for backwards compatibility; using positional arguments
+        # is NOT recommended. Use keyword arguments instead.
+        parser.add_argument('backend', default=None) # first positional argument
+        parser.add_argument('url', default=None) # second positional argument
+
         # Options for output database
-        group = OptionGroup(parser, 'Output database specific options')
-        group.add_option('--db-driver-out',
-                         choices=['sqlite','mysql','postgresql'],
-                         dest='db_driver_out', help='Output database driver',
-                         default='mysql')
-        group.add_option('--db-user-out', dest='db_user_out',
-                         help='Database user name', default=None)
-        group.add_option('--db-password-out', dest='db_password_out',
-                         help='Database user password', default=None)
-        group.add_option('--db-hostname-out', dest='db_hostname_out',
-                         help='Name of the host where database server is running',
-                         default='localhost')
-        group.add_option('--db-port-out', dest='db_port_out',
-                         help='Port of the host where database server is running',
-                         default='3306')
-        group.add_option('--db-database-out', dest='db_database_out',
-                         help='Output database name', default=None)
-        parser.add_option_group(group)
-    
+        group = parser.add_argument_group('Output database specific options')
+        group.add_argument('--db-driver-out',
+                           choices=['sqlite','mysql','postgresql'],
+                           dest='db_driver_out', help='Output database driver',
+                           default='mysql')
+        group.add_argument('--db-user-out', dest='db_user_out',
+                           help='Database user name', default=None)
+        group.add_argument('--db-password-out', dest='db_password_out',
+                           help='Database user password', default=None)
+        group.add_argument('--db-hostname-out', dest='db_hostname_out',
+                           help='Name of the host where database server is running',
+                           default='localhost')
+        group.add_argument('--db-port-out', dest='db_port_out',
+                           help='Port of the host where database server is running',
+                           default='3306')
+        group.add_argument('--db-database-out', dest='db_database_out',
+                           help='Output database name', default=None)
+
         # Options for input database
-        group = OptionGroup(parser, 'Input database specific options')
-        group.add_option('--db-driver-in',
-                         choices=['sqlite', 'mysql', 'postgresql'],
-                         dest='db_driver_in', help='Input database driver',
-                         default=None)
-        group.add_option('--db-user-in', dest='db_user_in',
-                         help='Database user name', default=None)
-        group.add_option('--db-password-in', dest='db_password_in',
-                         help='Database user password', default=None)
-        group.add_option('--db-hostname-in', dest='db_hostname_in',
-                         help='Name of the host where database server is running',
-                         default=None)
-        group.add_option('--db-port-in', dest='db_port_in',
-                         help='Port of the host where database server is running',
-                         default=None)
-        group.add_option('--db-database-in', dest='db_database_in',
-                         help='Input database name', default=None)
-        parser.add_option_group(group)
-                
-        (options, args) = parser.parse_args()
-                            
-        if options.cfgfile is not None:
-            Config.load_from_file(options.cfgfile)
+        group = parser.add_argument_group('Input database specific options')
+        group.add_argument('--db-driver-in',
+                           choices=['sqlite', 'mysql', 'postgresql'],
+                           dest='db_driver_in', help='Input database driver',
+                           default=None)
+        group.add_argument('--db-user-in', dest='db_user_in',
+                           help='Database user name', default=None)
+        group.add_argument('--db-password-in', dest='db_password_in',
+                           help='Database user password', default=None)
+        group.add_argument('--db-hostname-in', dest='db_hostname_in',
+                           help='Name of the host where database server is running',
+                           default=None)
+        group.add_argument('--db-port-in', dest='db_port_in',
+                           help='Port of the host where database server is running',
+                           default=None)
+        group.add_argument('--db-database-in', dest='db_database_in',
+                           help='Input database name', default=None)
+
+        args = parser.parse_args()
+
+        if args.cfgfile is not None:
+            Config.load_from_file(args.cfgfile)
         else:
             Config.load()
-                        
-        # Command line options have preference
-        # Backwards compatibility
-        if (len(args) > 0):
-            Config.backend = args[0]
-        if (len(args) == 2):
-            Config.url = args[1]
 
         # Not remove config file options with empty default values
-        Config.__dict__.update(Config.clean_empty_options(options))
+        Config.__dict__.update(Config.clean_empty_options(args))
         Config.check_config()
