e84e2084d3228dd3e525db07773f35a8a969a552
Santiago Dueñas <sduenas@bitergia.com>
2014-01-16 19:55:37 +0100

    [Redmine] Add Redmine URL generator
    This class is responsible of generating any URL the backend
    might need while crawling a Redmine issue tracking system.
    
    In addition, unit tests are added.

---
+++ bicho/backends/redmine/urls.py
@@ -0,0 +1,138 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2014 Bitergia
+# Copyright (C) 2007-2013 GSyC/LibreSoft, Universidad Rey Juan Carlos
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+# Authors:
+#         Santiago Dueñas <sduenas@bitergia.com>
+#
+
+"""
+URLs generator for Redmine tracker
+"""
+
+import urlparse
+import urllib
+
+from bicho.exceptions import InvalidBaseURLError
+
+
+# URL suffixes
+ISSUES_SUFFIX = 'issues'
+JSON_SUFFIX = '.json'
+PROJECT_SUFFIX = 'projects/'
+STATUSES_SUFFIX = 'issue_statuses'
+
+# URL parameters
+INCLUDE_PARAM = 'include'
+PAGE_PARAM = 'page'
+SORT_PARAM = 'sort'
+STATUS_PARAM = 'status_id'
+UPDATED_ON_PARAM = 'updated_on'
+
+# Values
+JOURNALS_VALUE = 'journals'
+UPDATED_ON_VALUE = 'updated_on'
+STATUS_VALUE = '*'
+
+
+class RedmineURLGenerator(object):
+    """Redmine's URLs generator
+
+     Builds the different URLs needed for crawling a Redmine tracker site.
+
+    :param base_url: URL used to build requested specific URLs
+    :type base_url: str
+    :param version: version of the tracker
+    :type version: str
+
+    :raise InvalidBaseURLError: when base_url parameter is not valid
+    """
+    def __init__(self, base_url, version=None):
+        self.base_url = base_url
+        self.version = version
+
+    @property
+    def base_url(self):
+        return self._url_parts.geturl()
+
+    @base_url.setter
+    def base_url(self, value):
+        self._url_parts = self._parse_base_url(value)
+
+    def get_issues_summary_url(self, product=None, from_date=None, page=None):
+        qs = {STATUS_PARAM: STATUS_VALUE,
+              SORT_PARAM: UPDATED_ON_VALUE}
+
+        if product:
+            suffix = PROJECT_SUFFIX + product + '/' + ISSUES_SUFFIX + JSON_SUFFIX
+        else:
+            suffix = ISSUES_SUFFIX + JSON_SUFFIX
+
+        if from_date:
+            qs[UPDATED_ON_PARAM] = '>=' + from_date
+        if page:
+            qs[PAGE_PARAM] = page
+
+        return self._get_url(suffix, qs)
+
+    def get_issue_url(self, issue_id):
+        if not issue_id:
+            raise ValueError('issue_id cannot be None or empty')
+
+        qs = {INCLUDE_PARAM: JOURNALS_VALUE}
+
+        suffix = ISSUES_SUFFIX + '/' + issue_id + JSON_SUFFIX
+
+        return self._get_url(suffix, qs)
+
+    def get_statuses_url(self):
+        suffix = STATUSES_SUFFIX + JSON_SUFFIX
+        return self._get_url(suffix, {})
+
+    def _parse_base_url(self, url):
+        if not url:
+            msg = 'Empty URL'
+            raise InvalidBaseURLError(url=url, cause=msg)
+
+        parts = urlparse.urlparse(url)
+
+        # Possible errors
+        if parts.query:
+            msg = 'Query parameters found'
+            raise InvalidBaseURLError(url=url, cause=msg)
+        elif parts.path[-5:] == '.json':
+            msg = 'URL related to a JSON'
+            raise InvalidBaseURLError(url=url, cause=msg)
+
+        # Add trailing slash to avoid future problems joining paths with
+        # urljoin function. Without this slash, the path of the base URL
+        # will be ignored and overwritten by the second parameter in urljoin.
+        path = parts.path
+        if parts.path[-1] != '/':
+            path = parts.path + '/'
+
+        parts = urlparse.ParseResult(parts.scheme, parts.netloc, path,
+                                     None, None, None)
+        return parts
+
+    def _get_url(self, suffix, qs):
+        query = urllib.urlencode(qs, True)
+        url = urlparse.ParseResult(self._url_parts.scheme, self._url_parts.netloc,
+                                   self._url_parts.path + suffix,
+                                   None, query, None)
+        return url.geturl()
---
+++ tests/test_redmine_urls.py
@@ -0,0 +1,128 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2014 Bitergia
+# Copyright (C) 2011-2013 GSyC/LibreSoft, Universidad Rey Juan Carlos
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+# Authors:
+#         Santiago Dueñas <sduenas@bitergia.com>
+#
+
+import sys
+import unittest
+
+if not '..' in sys.path:
+    sys.path.insert(0, '..')
+
+from bicho.exceptions import InvalidBaseURLError
+from bicho.backends.redmine.urls import RedmineURLGenerator
+
+
+# Test case URLs
+REDMINE_URL = 'http://example.org/redmine/'
+REDMINE_URL_WITHOUT_SLASH = 'http://example.org/redmine'
+ISSUE_TRACKER_URL = 'http://example.org/redmine/'
+QUERY_URL = 'http://example.org/redmine/issues.json?status_id=*&page=77'
+JSON_URL = 'http://example.org/redmine/issues.json?'
+JSON_SUBSTRING_URL = 'http://example.json/.jsons/'
+
+# Test case dates
+ISSUE_DATE = '2001-01-01'
+ISSUE_TIMESTAMP = '2003-06-27 22:10:08'
+
+
+class TestRedmineURLGenerator(unittest.TestCase):
+
+    def test_issues_summary_url(self):
+        generator = RedmineURLGenerator(REDMINE_URL)
+        self.assertEqual('http://example.org/redmine/issues.json?sort=updated_on&status_id=%2A',
+                         generator.get_issues_summary_url())
+
+    def test_issues_summary_url_with_params(self):
+        generator = RedmineURLGenerator(REDMINE_URL)
+        self.assertEqual('http://example.org/redmine/issues.json?sort=updated_on&updated_on=%3E%3D2001-01-01&status_id=%2A',
+                         generator.get_issues_summary_url(from_date=ISSUE_DATE))
+        self.assertEqual('http://example.org/redmine/projects/fake-product/issues.json?sort=updated_on&status_id=%2A',
+                         generator.get_issues_summary_url(product='fake-product'))
+        self.assertEqual('http://example.org/redmine/issues.json?sort=updated_on&page=3&status_id=%2A',
+                         generator.get_issues_summary_url(page='3'))
+        self.assertEqual('http://example.org/redmine/projects/fake-product/issues.json?sort=updated_on&updated_on=%3E%3D2001-01-01&page=3&status_id=%2A',
+                         generator.get_issues_summary_url(product='fake-product', from_date=ISSUE_DATE, page='3'))
+
+    def test_issue_url(self):
+        generator = RedmineURLGenerator(REDMINE_URL)
+        self.assertEqual('http://example.org/redmine/issues/88.json?include=journals',
+                         generator.get_issue_url('88'))
+
+    def test_issues_description_url_invalid_list(self):
+        generator = RedmineURLGenerator(REDMINE_URL)
+        self.assertRaisesRegexp(ValueError, 'issue_id cannot be None or empty',
+                                generator.get_issue_url, issue_id=None)
+        self.assertRaisesRegexp(ValueError, 'issue_id cannot be None or empty',
+                                generator.get_issue_url, issue_id='')
+
+    def test_statuses_url(self):
+        generator = RedmineURLGenerator(REDMINE_URL)
+        self.assertEqual('http://example.org/redmine/issue_statuses.json',
+                         generator.get_statuses_url())
+
+    def test_invalid_urls(self):
+        # Test if the generator fails when a new instance is created
+        # passing invalid URLs
+        self.assertRaisesRegexp(InvalidBaseURLError, 'Empty URL',
+                                RedmineURLGenerator, base_url=None)
+        self.assertRaisesRegexp(InvalidBaseURLError, 'Empty URL',
+                                RedmineURLGenerator, base_url='')
+        self.assertRaisesRegexp(InvalidBaseURLError, 'Query parameters found',
+                                RedmineURLGenerator, base_url=QUERY_URL)
+        self.assertRaisesRegexp(InvalidBaseURLError, 'URL related to a JSON',
+                                RedmineURLGenerator, base_url=JSON_URL)
+
+        # Do not raise any error because it's a valid URL
+        generator = RedmineURLGenerator(JSON_SUBSTRING_URL)
+        self.assertEqual(JSON_SUBSTRING_URL, generator.base_url)
+
+    def test_trailing_slash(self):
+        # Test if a trailing slash is added to base URL
+        generator = RedmineURLGenerator(REDMINE_URL_WITHOUT_SLASH)
+        self.assertEqual(REDMINE_URL, generator.base_url)
+
+    def test_assign_base_url(self):
+        # Test whether fails or not assigning invalid URLs
+        generator = RedmineURLGenerator(REDMINE_URL)
+
+        self.assertRaisesRegexp(InvalidBaseURLError, 'Empty URL',
+                                setattr, generator, 'base_url', None)
+        self.assertEqual(REDMINE_URL, generator.base_url)
+
+        self.assertRaisesRegexp(InvalidBaseURLError, 'Empty URL',
+                                setattr, generator, 'base_url', '')
+        self.assertEqual(REDMINE_URL, generator.base_url)
+
+        self.assertRaisesRegexp(InvalidBaseURLError, 'Query parameters found',
+                                setattr, generator, 'base_url', QUERY_URL)
+        self.assertEqual(REDMINE_URL, generator.base_url)
+
+        self.assertRaisesRegexp(InvalidBaseURLError, 'URL related to a JSON',
+                                setattr, generator, 'base_url', JSON_URL)
+        self.assertEqual(REDMINE_URL, generator.base_url)
+
+        generator.base_url = ISSUE_TRACKER_URL
+        self.assertEqual(ISSUE_TRACKER_URL, generator.base_url)
+
+
+if __name__ == "__main__":
+    unittest.main()
